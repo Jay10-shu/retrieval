8ceb756ec64ebdc4687016fae5728fd2bca3c98b
Jay10-shu <156511900+Jay10-shu@users.noreply.github.com>
2025-10-13 15:29:49 +0800

    Update run_msrvtt.sh

--- scripts/run_msrvtt.sh
+++ scripts/run_msrvtt.sh
@@ -1,17 +1,17 @@
-export MASTER_PORT=6115
-export CUDA_VISIBLE_DEVICES=0
+export MASTER_PORT=6110
+export CUDA_VISIBLE_DEVICES=2
 export OMP_NUM_THREADS=2
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 export LOCAL_RANK=0
 
 
 DATA_PATH="/home/mfy/code/PAU-main/data"
-SAVE_PATH="log-msrvtt"
+SAVE_PATH="log-msrvtt1"
 python -m torch.distributed.launch --nproc_per_node=1 --master_port $MASTER_PORT \
-    main_DLAR.py --do_train  --num_thread_reader=10 \
-    --lr 1e-4 --batch_size=64 --batch_size_val 100 \
-    --epochs=5  --n_display=100\
-    --train_csv ${DATA_PATH}/msrvtt_data/MSRVTT_train.9k.csv\
+    main_DLAR1.py --do_train  --num_thread_reader=10 \
+    --lr 1e-4 --batch_size=96 --batch_size_val 100 \
+    --epochs=5  --n_display=50\
+    --train_csv ${DATA_PATH}/msrvtt_data//MSRVTT_train.9k.csv\
     --val_csv ${DATA_PATH}/msrvtt_data/MSRVTT_JSFUSION_test.csv \
     --data_path ${DATA_PATH}/msrvtt_data/MSRVTT_data.json \
     --features_path ${DATA_PATH}/msrvtt_data/MSRVTT_Videos \
@@ -23,8 +23,5 @@ python -m torch.distributed.launch --nproc_per_node=1 --master_port $MASTER_PORT
     --tau 20  \
     --lambda1 1 --lambda2 1 \
     --loose_type --linear_patch 2d --sim_header seqTransf \
-    --num_clusters 15 \
-    --fuzzy_index 1.1 \
-    # --extract_feature \
-
-  
+    --num_clusters 100 \
+    --fuzzy_index 1.1
